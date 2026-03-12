7408d519c5361fcb9c23e5ec893badcf0b52a5b8
Anuva Singh <singhanuva4@gmail.com>
2026-03-12 22:13:00 +0530

    data_preprocessing file

--- preprocessing/data_preprocessing.py
+++ preprocessing/data_preprocessing.py
@@ -1,19 +1,12 @@
 import pandas as pd
 
 
-def load_data(path):
-
-    df = pd.read_csv(path)
-
-    return df
-
-
-def compute_travel_times(df):
+def preprocess_data(df):
 
     df['arrival_time'] = pd.to_datetime(df['arrival_time'])
     df['departure_time'] = pd.to_datetime(df['departure_time'])
 
-    df = df.sort_values(['trip_id', 'arrival_time'])
+    df = df.sort_values(['trip_id','arrival_time'])
 
     edges = []
 
@@ -24,33 +17,18 @@ def compute_travel_times(df):
         for i in range(len(group)-1):
 
             current = group.iloc[i]
-            nxt = group.iloc[i+1]
+            next_stop = group.iloc[i+1]
 
             travel_time = (
-                nxt['arrival_time'] - current['departure_time']
+                next_stop['arrival_time'] - current['departure_time']
             ).total_seconds()
 
-            travel_time = max(travel_time, 1)
-
-            # forward edge
             edges.append({
                 "from_stop": current["stop_id"],
-                "to_stop": nxt["stop_id"],
-                "travel_time": travel_time
-            })
-
-            # reverse edge
-            edges.append({
-                "from_stop": nxt["stop_id"],
-                "to_stop": current["stop_id"],
-                "travel_time": travel_time
+                "to_stop": next_stop["stop_id"],
+                "travel_time": max(travel_time,1)
             })
 
     edges_df = pd.DataFrame(edges)
 
-    # average travel time across trips
-    edges_df = edges_df.groupby(
-        ["from_stop", "to_stop"]
-    )["travel_time"].mean().reset_index()
-
-    return edges_df
+    return df, edges_df
